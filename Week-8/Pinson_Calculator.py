9ca1de294dedbc841c3603449c93b87e03f19b7b
James <jpinson@my365.bellevue.edu>
2021-05-04 20:19:16 -0500

    Week 8

---
+++ Week-8/Pinson_Calculator.py
@@ -0,0 +1,36 @@
+# ============================
+# Title: Pinson_Calculator
+# Author: James Pinson
+# Date: May 4th 2021
+# Description: Create an add, subtract, and divide function using Python
+# =============================
+
+first_name = 'James'
+last_name = 'Pinson'
+assignment = 'Exercise 8.3'
+
+print(first_name + ' ' + last_name + '\n' + assignment)
+
+# This is the add function
+
+
+def add(add1, add2):
+    return add1 + add2
+
+# This is the subtract function
+
+
+def subtract(sub1, sub2):
+    return sub1 - sub2
+
+# This is the divide function.
+
+
+def divide(div1, div2):
+    return div1 / div2
+
+
+# This is the output for our answers.
+print(add(1, 2))
+print(subtract(4, 1))
+print(divide(8, 2))
